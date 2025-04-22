864642697aa7ce00b74f28c0d16ffeeb2606c36a
Sanjay-adepu <adepusanjay812@gmail.com>
2025-04-22 18:41:58 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("https://adepu-sanjay-svf9.vercel.app/contact", formData);
+            const res = await axios.post("https://adepu-sanjay-svf9.vercel.app/api/contact", formData);
             if (res.data.success) {
                 setSuccess("Message sent successfully!");
                 setFormData({ name: "", email: "", message: "" });
